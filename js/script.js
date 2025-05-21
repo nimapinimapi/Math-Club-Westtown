58939ee3a236b1a48db291ba4538b2a12180cdb4
nimapinimapi <78689374+nimapinimapi@users.noreply.github.com>
2025-05-21 09:30:44 -0400

    Files

--- js/script.js
+++ js/script.js
@@ -1,4 +1,40 @@
+/* Navigation Bar Script */
 $("#menu").click(function() {
     $("#navigation-bar > div").slideToggle();
+    $("#menu").toggleClass("rotate");
+});
 
+/* Checking Mechanism for Problem 1 */
+$("#check1").click(function() {
+    $("#problem1 .emojis").css("display", "none");
+    if(document.getElementById("problem1answer").value=="B" || document.getElementById("problem1answer").value=="b") {
+        $("#checkmark1").css("display", "inline");
+    }
+    else {
+        $("#wrong1").css("display", "inline");
+    }
+});
+
+/* Checking Mechanism for Problem 2 */
+
+$("#check2").click(function() {
+    $("#problem2 .emojis").css("display", "none");
+    if(document.getElementById("problem2answer").value==55) {
+        $("#checkmark2").css("display", "inline");
+    }
+    else {
+        $("#wrong2").css("display", "inline");
+    }
+});
+
+/* Checking Mechanism for Problem 3 */
+
+$("#check3").click(function() {
+    $("#problem3 .emojis").css("display", "none");
+    if(document.getElementById("problem3answer").value==61) {
+        $("#checkmark3").css("display", "inline");
+    }
+    else {
+        $("#wrong3").css("display", "inline");
+    }
 });
