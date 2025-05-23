0f7510149ba4a664fca5a5ee1d1f2332e2e1ba48
nimapinimapi <78689374+nimapinimapi@users.noreply.github.com>
2025-05-23 11:43:43 -0400

    Pages

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ $("#menu").click(function() {
 /* Checking Mechanism for Problem 1 */
 $("#check1").click(function() {
     $("#problem1 .emojis").css("display", "none");
-    if(document.getElementById("problem1answer").value=="B" || document.getElementById("problem1answer").value=="b") {
+    if(document.getElementById("problem1answer").value=="B" || document.getElementById("problem1answer").value=="b" || document.getElementById("problem1answer").value=="0") {
         $("#checkmark1").css("display", "inline");
     }
     else {
@@ -16,7 +16,6 @@ $("#check1").click(function() {
 });
 
 /* Checking Mechanism for Problem 2 */
-
 $("#check2").click(function() {
     $("#problem2 .emojis").css("display", "none");
     if(document.getElementById("problem2answer").value==55) {
@@ -28,7 +27,6 @@ $("#check2").click(function() {
 });
 
 /* Checking Mechanism for Problem 3 */
-
 $("#check3").click(function() {
     $("#problem3 .emojis").css("display", "none");
     if(document.getElementById("problem3answer").value==61) {
@@ -37,4 +35,28 @@ $("#check3").click(function() {
     else {
         $("#wrong3").css("display", "inline");
     }
+});
+
+
+// Create an animation for the winning scores //
+const tds = document.getElementsByTagName("td");
+
+for (let i = 0; i < tds.length; i++) {
+    const cell = tds[i];
+    const firstChar = cell.textContent.trim().charAt(0);
+
+    if (firstChar === "W") {
+        cell.style.color = "green";
+        cell.classList.add("we-won");
+    } else if (firstChar === "L") {
+        cell.style.color = "red";
+    }
+}
+
+//Button to stop the animation//
+$("#stopWin").click(function () {
+    //remove all instances of the animation for the class we-won//
+    document.querySelectorAll(".we-won").forEach(cell => {
+        cell.classList.remove("we-won");
+    });
 });
